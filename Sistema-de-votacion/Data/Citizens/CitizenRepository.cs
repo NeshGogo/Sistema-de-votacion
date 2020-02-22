99bfbb338f1d89fb9ce764ccaf3cadf7f4e5aa2a
Rafael Aguero Baez <rafaelaguero10@hotmail.com>
2020-02-21 22:24:09 -0400

    Cree el repositorio para citizen, que se encarga del manejo de los ciudadanos.

---
+++ Sistema-de-votacion/Data/Citizens/CitizenRepository.cs
@@ -0,0 +1,16 @@
+﻿using Sistema_de_votacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_votacion.Data.Citizens
+{
+    public class CitizenRepository : SQLRepository<Citizen>, ICitizenRepository
+    {
+        public CitizenRepository(ElectionDBContext context): base(context)
+        {
+
+        }
+    }
+}
