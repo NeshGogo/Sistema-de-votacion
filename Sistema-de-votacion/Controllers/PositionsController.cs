185216e1c0a71fb1d93a08c4068902543519c4ce
NeshGogo <rafaelaguero10@hotmail.com>
2020-02-25 22:24:09 -0400

    Controlador y vistas iniciales de position
    Se creo el controlador base para positions al igual que sus vistas. Tambien se realizo la inyeccion de dependencia en el controlador del servicios de positios para el acceso a los datos.

---
+++ Sistema-de-votacion/Controllers/PositionsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sistema_de_votacion.Data;
+using Sistema_de_votacion.Models;
+using Sistema_de_votacion.Services.Candidates.Positions;
+
+namespace Sistema_de_votacion.Controllers
+{
+    public class PositionsController : Controller
+    {
+        private readonly IPositionService _positionService;
+
+        public PositionsController(IPositionService positionService)
+        {
+            _positionService = positionService;
+        }
+
+        // GET: Positions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _positionService.GetPositions().ToListAsync());
+        }
+
+        // GET: Positions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var position = await Task.FromResult(_positionService.GetPositionById(id.Value));
+                
+            if (position == null)
+            {
+                return NotFound();
+            }
+
+            return View(position);
+        }
+
+        // GET: Positions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Positions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,IsActive")] Position position)
+        {
+            if (ModelState.IsValid)
+            {
+               await  Task.FromResult(_positionService.InsertPosition(position));
+               
+                return RedirectToAction(nameof(Index));
+            }
+            return View(position);
+        }
+
+        // GET: Positions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var position = await Task.FromResult(_positionService.GetPositionById(id.Value));
+            if (position == null)
+            {
+                return NotFound();
+            }
+            return View(position);
+        }
+
+        // POST: Positions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,IsActive")] Position position)
+        {
+            if (id != position.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await Task.FromResult(_positionService.UdatePosition(position));
+                    
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PositionExists(position.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(position);
+        }
+
+        // GET: Positions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var position =  await Task.FromResult( _positionService.GetPositionById(id.Value) );
+            
+                
+            if (position == null)
+            {
+                return NotFound();
+            }
+
+            return View(position);
+        }
+
+        // POST: Positions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var position = _positionService.GetPositionById(id);
+            await Task.FromResult( _positionService.DeletePosition(position));
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PositionExists(int id)
+        {
+            return _positionService.GetPositions().Any(e => e.Id == id);
+        }
+    }
+}
